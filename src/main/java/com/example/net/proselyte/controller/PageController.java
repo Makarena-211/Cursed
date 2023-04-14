50728e77b969e7114b4178dd04c29f95be0eb474
Makarena-211 <mnfomenkov@gmail.com>
2023-04-14 13:57:57 +0300

    first commit

--- src/main/java/com/example/net/proselyte/controller/PageController.java
+++ src/main/java/com/example/net/proselyte/controller/PageController.java
@@ -1,7 +1,11 @@
 package com.example.net.proselyte.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.print.attribute.standard.Media;
 
 @Controller
 public class PageController {
